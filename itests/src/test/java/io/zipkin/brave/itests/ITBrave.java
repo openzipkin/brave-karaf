9a34b0aa3448a7ec7f5ae25f5736fc30a17581cb
Andriy Redko <drreta@gmail.com>
2024-12-02 20:41:10 -0500

    Update to Brave 6 / Zipkin 3
    Signed-off-by: Andriy Redko <drreta@gmail.com>

--- itests/src/test/java/io/zipkin/brave/itests/ITBrave.java
+++ itests/src/test/java/io/zipkin/brave/itests/ITBrave.java
@@ -1,15 +1,6 @@
 /*
- * Copyright 2016-2020 The OpenZipkin Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Copyright The OpenZipkin Authors
+ * SPDX-License-Identifier: Apache-2.0
  */
 package io.zipkin.brave.itests;
 
@@ -38,7 +29,8 @@ import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
-import zipkin2.reporter.Sender;
+
+import zipkin2.reporter.BytesMessageSender;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -89,23 +81,23 @@ public class ITBrave {
   }
 
   @Test public void checkSenderUrlConnection() {
-    ServiceReference<Sender> ref =
-        getSingle(Sender.class, "(component.name=io.zipkin.sender.urlconnection)");
+    ServiceReference<BytesMessageSender> ref =
+        getSingle(BytesMessageSender.class, "(component.name=io.zipkin.sender.urlconnection)");
 
     assertPropertyEquals(ref, "endpoint", "http://localhost:9411/api/v2/spans");
     assertPropertyEquals(ref, "connectTimeout", 10000);
   }
 
   @Test public void checkSenderKafka() {
-    ServiceReference<Sender> ref =
-        getSingle(Sender.class, "(component.name=io.zipkin.sender.kafka)");
+    ServiceReference<BytesMessageSender> ref =
+        getSingle(BytesMessageSender.class, "(component.name=io.zipkin.sender.kafka)");
 
     assertPropertyEquals(ref, "topic", "zipkin");
   }
 
   @Test public void checkSenderOkHttp() {
-    ServiceReference<Sender> ref =
-        getSingle(Sender.class, "(component.name=io.zipkin.sender.okhttp)");
+    ServiceReference<BytesMessageSender> ref =
+        getSingle(BytesMessageSender.class, "(component.name=io.zipkin.sender.okhttp)");
 
     assertPropertyEquals(ref, "endpoint", "http://localhost:9411/api/v2/spans");
   }
